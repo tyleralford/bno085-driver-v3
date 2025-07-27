0ebc2b55887dd023ac9cba887c44c3661e15180c
tyleralford <alford.tyler94@gmail.com>
2025-07-27 15:33:20 -0500

    cleanup

--- bno085_driver/__init__.py
+++ bno085_driver/__init__.py
@@ -2,5 +2,5 @@
 
 __version__ = "1.0.0"
 __author__ = "Tyler Alford"
-__email__ = "tyler@example.com"
-__description__ = "A high-performance micro-ROS driver for the Adafruit BNO085 9-DOF IMU sensor"
+__email__ = "alford.tyler94@gmail.com"
+__description__ = "A hmicro-ROS driver for the BNO085 9-DOF IMU sensor"
